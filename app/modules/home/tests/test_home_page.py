2bf3cc25f38a69e4d0385e521e7eeeb667dbfac1
Andy Beaumont <andy@andybeaumont.com>
2020-03-31 12:42:12 +0100

    Configures the templates location and adds a test for loading the home page. This test currently fails as we haven't configured static assets yet.

--- app/modules/home/tests/test_home_page.py
+++ app/modules/home/tests/test_home_page.py
@@ -12,3 +12,10 @@ def test_home_page_gets_created(home_page):
     """
     assert home_page is not None
     assert home_page.title == 'Test Site Home'
+
+
+def test_home_page_200(home_page):
+    """Test that we have a home page created by the fixture
+    """
+    rv = client.get(home_page.url)
+    assert rv.status_code == 200
