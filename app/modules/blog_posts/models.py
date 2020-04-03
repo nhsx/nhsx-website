88d64cba7cc7476b52d24c24aa7e174328787fc9
Stuart Harrison <pezholio@gmail.com>
2020-04-03 10:44:39 +0100

    Only allow one index page of each type

--- app/modules/blog_posts/models.py
+++ app/modules/blog_posts/models.py
@@ -2,6 +2,7 @@ from modules.core.models.abstract import BasePage, BaseIndexPage
 
 class BlogPostIndexPage(BaseIndexPage):
     subpage_types = [ 'BlogPost' ]
+    max_count = 1
 
 class BlogPost(BasePage):
     parent_page_types = ['BlogPostIndexPage']
--- app/modules/news/models.py
+++ app/modules/news/models.py
@@ -2,6 +2,7 @@ from modules.core.models.abstract import BasePage, BaseIndexPage
 
 class NewsIndexPage(BaseIndexPage):
     subpage_types = ['News']
+    max_count = 1
 
 class News(BasePage):
     parent_page_types = ['NewsIndexPage']
