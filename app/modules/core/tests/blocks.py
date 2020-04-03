df14ec4815940aedaaa2b3c622eb4c3cc3b3b1a1
Andy Beaumont <andy@andybeaumont.com>
2020-04-03 12:38:00 +0100

    Forgot to add the blocks file.

---
+++ app/modules/core/tests/blocks.py
@@ -0,0 +1,9 @@
+RICHTEXT_BLOCK = {
+    'type': 'rich_text',
+    'value': """Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit
+    aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+    massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras
+    justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis
+    ornare vel eu leo."""
+}
