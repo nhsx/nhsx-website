0bea521c7bcc783a2773b6ae637fea6e7752b302
Stuart Harrison <pezholio@gmail.com>
2020-04-03 15:34:18 +0100

    Add tags to blog post model

--- app/modules/blog_posts/models.py
+++ app/modules/blog_posts/models.py
@@ -1,9 +1,30 @@
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+from taggit.models import TaggedItemBase
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from wagtail.admin.edit_handlers import FieldPanel
+
 from modules.core.models.abstract import BasePage, BaseIndexPage
 
 class BlogPostIndexPage(BaseIndexPage):
     subpage_types = [ 'BlogPost' ]
     max_count = 1
 
+class BlogTag(TaggedItemBase):
+	content_object = ParentalKey(
+	    'BlogPost',
+	    related_name='tagged_items',
+	    on_delete=models.CASCADE,
+	)
+
 class BlogPost(BasePage):
     parent_page_types = ['BlogPostIndexPage']
 
+    tags = ClusterTaggableManager(through=BlogTag, blank=True)
+
+    promote_panels = [
+        *BasePage.page_promote_panels,
+        FieldPanel("tags"),
+        *BasePage.social_promote_panels
+    ]
