72e9b656d6e7b088c457b44c2f91307242029865
Andy Beaumont <andy@andybeaumont.com>
2020-04-07 15:08:28 +0100

    Wrap urls with wagtail-cache

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,14 +1,20 @@
+# stdlib
 import os
-from django.http import HttpResponse
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
 
+# 3rd party
+from django.conf import settings
+from django.http import HttpResponse
+from wagtail.core import views as wagtail_views
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
+from django.contrib import admin
+from django.conf.urls import url, include
+from wagtail.core.urls import WAGTAIL_FRONTEND_LOGIN_TEMPLATE, serve_pattern
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtailcache.cache import cache_page
+from django.contrib.auth import views as auth_views
 from wagtail.contrib.sitemaps.views import sitemap
 
+# Project
 from modules.search import views as search_views
 from modules.core.views import AuthorAutocomplete
 
@@ -57,12 +63,17 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    url(r"", include(wagtail_urls)),
+    url(
+        r'^_util/authenticate_with_password/(\d+)/(\d+)/$',
+        wagtail_views.authenticate_with_password,
+        name='wagtailcore_authenticate_with_password'
+    ),
+    url(
+        r'^_util/login/$',
+        auth_views.LoginView.as_view(template_name=WAGTAIL_FRONTEND_LOGIN_TEMPLATE),
+        name='wagtailcore_login'
+    ),
 
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r"^pages/", include(wagtail_urls)),
+    # Wrap the serve function with wagtail-cache
+    url(serve_pattern, cache_page(wagtail_views.serve), name='wagtail_serve'),
 ]
