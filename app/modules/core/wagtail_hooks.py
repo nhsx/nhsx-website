b209ddfa6d80f02c78302af81700cbabede62d46
Andy Beaumont <andy@andybeaumont.com>
2020-04-08 16:54:01 +0100

    Invalidate cacheops cache on any page save operation.

---
+++ app/modules/core/wagtail_hooks.py
@@ -0,0 +1,10 @@
+from cacheops import invalidate_all
+from wagtailcache.cache import clear_cache
+
+
+@hooks.register('after_create_page')
+@hooks.register('after_edit_page')
+def clear_wagtailcache(request, page):
+    invalidate_all()
+    if page.live:
+        clear_cache()
