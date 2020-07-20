bc3302bb72df9558ac345d1269b7342335bd0a54
Stuart Harrison <pezholio@gmail.com>
2020-07-20 14:50:04 +0100

    Restructure how resources are listed
    This removes the Use Case tag, and allows resources to be listed under a child
    index page. This simplifies how use cases are added and allows us to have more
    detail for a particular use case

--- app/modules/ai_lab/models.py
+++ app/modules/ai_lab/models.py
@@ -1,11 +1,7 @@
 from django import forms
 from django.db import models
-from django.shortcuts import render
-from django.utils.text import slugify
-from django.http import Http404
 
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
-from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.models import Page
 
 from modules.core.models.abstract import BasePage
@@ -28,39 +24,11 @@ class AiLabHomePage(SectionPage):
         return context
 
 
-class AiLabUseCase(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=200, null=True, unique=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = self._get_unique_slug()
-        return super().save(*args, **kwargs)
-
-    def _get_unique_slug(self):
-        slug = slugify(self.name)
-        unique_slug = slug
-        counter = 1
-        while self.__class__.objects.filter(slug=unique_slug).exists():
-            if (
-                self.__class__.objects.filter(slug=unique_slug).values("id")[0]["id"]
-                == id
-            ):
-                break
-            unique_slug = "{}-{}".format(slug, counter)
-            counter += 1
-        return unique_slug
-
-    def __str__(self):
-        return self.name
-
-
 class AiLabResourceMixin(models.Model):
-    parent_page_types = ["AiLabResourceIndexPage"]
-    use_case = models.ForeignKey(AiLabUseCase, on_delete=models.PROTECT)
-    content_panels = ArticlePage.content_panels + [
-        FieldPanel("use_case", widget=forms.Select())
+    parent_page_types = [
+        "AiLabUnderstandIndexPage",
+        "AiLabDevelopIndexPage",
+        "AiLabAdoptIndexPage",
     ]
 
     class Meta:
@@ -75,14 +43,21 @@ class AiLabExternalResource(AiLabResourceMixin, Page):
     external_url = models.URLField()
     content_panels = Page.content_panels + [
         FieldPanel("external_url", widget=forms.URLInput()),
-        FieldPanel("use_case", widget=forms.Select()),
     ]
 
 
-class AiLabResourceIndexPage(RoutablePageMixin, BasePage):
+class AiLabResourceIndexPage(BasePage):
+    """
+    An index page that lists all resources listed in the child
+    sections
+    """
+
     parent_page_types = ["AiLabHomePage"]
-    subpage_types = ["AiLabCaseStudy", "AiLabExternalResource"]
-    max_count = 1
+    subpage_types = [
+        "AiLabUnderstandIndexPage",
+        "AiLabDevelopIndexPage",
+        "AiLabAdoptIndexPage",
+    ]
 
     sub_head = models.CharField(max_length=255, blank=True, null=True)
 
@@ -92,48 +67,61 @@ class AiLabResourceIndexPage(RoutablePageMixin, BasePage):
         StreamFieldPanel("body"),
     ]
 
-    @route(r"^$")
-    def all_resources(self, request):
-        children = self.get_children().specific()
+    def get_context(self, request):
+        context = super().get_context(request)
 
-        return render(
-            request,
-            "ai_lab/ai_lab_resource_index_page.html",
-            {
-                "use_cases": AiLabUseCase.objects.all(),
-                "page": self,
-                "children": children,
-            },
-        )
+        resources = []
+        for child in self.get_children().specific():
+            for resource in child.get_children().specific():
+                resources.append(resource)
 
-    @route(r"^use-case/([a-z0-9]+(?:-[a-z0-9]+)*)/$")
-    def filter_by_use_case(self, request, slug):
-        ids = self._get_resource_ids_for_use_case(slug)
-        children = Page.objects.filter(id__in=(ids)).specific()
+        context["resources"] = sorted(resources, key=lambda r: r.title)
 
-        if children:
-            return render(
-                request,
-                "ai_lab/ai_lab_resource_index_page.html",
-                {"use_case": children[0].use_case, "page": self, "children": children,},
-            )
-        else:
-            raise Http404
+        return context
 
-    def _get_resource_ids_for_use_case(self, slug):
-        ids = []
-        for resource_ids_for_use_case in [
-            self._get_ids_for_class(klass, slug) for klass in self.subpage_types
-        ]:
-            for id in resource_ids_for_use_case:
-                ids.append(id)
 
-        return ids
+class AiLabCategoryIndexPageMixin(SectionPage):
+    """
+    A Mixin to be used by the category listing index pages
+    """
 
-    def _get_ids_for_class(self, klass, slug):
-        return (
-            eval(klass)
-            .objects.child_of(self)
-            .filter(use_case__slug=slug)
-            .values_list("id", flat=True)
-        )
+    parent_page_types = ["AiLabResourceIndexPage"]
+    subpage_types = ["AiLabCaseStudy", "AiLabExternalResource"]
+    max_count = 1
+
+    summary_title = models.CharField(
+        max_length=100,
+        help_text="The title that will appear on the resource index page",
+    )
+    summary_body = models.CharField(
+        max_length=255,
+        help_text="The description that will appear on the resource index page",
+    )
+
+    content_panels = [
+        FieldPanel("title"),
+        FieldPanel("sub_head"),
+        FieldPanel("summary_title"),
+        FieldPanel("summary_body"),
+    ]
+
+    def get_template(self, request):
+        return "ai_lab/ai_lab_category_index_page.html"
+
+    class Meta:
+        abstract = True
+
+
+class AiLabUnderstandIndexPage(AiLabCategoryIndexPageMixin):
+    class Meta:
+        verbose_name = "Understanding AI Index Page"
+
+
+class AiLabDevelopIndexPage(AiLabCategoryIndexPageMixin):
+    class Meta:
+        verbose_name = "Developing AI Index Page"
+
+
+class AiLabAdoptIndexPage(AiLabCategoryIndexPageMixin):
+    class Meta:
+        verbose_name = "Adopting AI Index Page"
