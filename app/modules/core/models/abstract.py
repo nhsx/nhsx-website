ba392d052daee913ccbce4abc60c31d6a789c2ba
Stuart Harrison <pezholio@gmail.com>
2020-07-24 09:14:04 +0100

    Only fetch live pages when showing subnav

--- app/modules/core/models/abstract.py
+++ app/modules/core/models/abstract.py
@@ -123,7 +123,7 @@ class SubNavMixin(PageLinksMixin):
 
     @cached_property
     def _children(self):
-        children = self.get_children()
+        children = self.get_children().live()
         return [{'title': _.title, 'url': _.url} for _ in children]
 
     @cached_property
