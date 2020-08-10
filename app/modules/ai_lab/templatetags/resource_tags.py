10a63d1daa1dd1919e1a0fe08aca1b47705b4acc
Stuart Harrison <pezholio@gmail.com>
2020-08-10 13:26:17 +0100

    Order subcategories in a specific way

--- app/modules/ai_lab/templatetags/resource_tags.py
+++ app/modules/ai_lab/templatetags/resource_tags.py
@@ -21,3 +21,19 @@ def resource_target(resource):
 def link_to_resource_type(resource, page):
     slug = slugify(resource._meta.verbose_name)
     return page.url + page.reverse_subpage("filter_by_type", args=[slug])
+
+
+@register.filter
+def ordered_subcategories(page):
+    children = page.get_children().specific()
+
+    return [
+        filter_by_type(children, "AiLabUnderstandIndexPage"),
+        filter_by_type(children, "AiLabDevelopIndexPage"),
+        filter_by_type(children, "AiLabAdoptIndexPage"),
+    ]
+
+
+def filter_by_type(children, type):
+    filtered_list = list([c for c in children if c.__class__.__name__ == type])
+    return filtered_list[0] if filtered_list else None
