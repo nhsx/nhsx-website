5ad9983619affc01f32afd77e3c389f826bc9569
Stuart Harrison <pezholio@gmail.com>
2020-08-10 12:02:39 +0100

    Make sure external resources open in a new tab

--- app/modules/ai_lab/templatetags/resource_tags.py
+++ app/modules/ai_lab/templatetags/resource_tags.py
@@ -10,6 +10,14 @@ def resource_type(resource):
 
 
 @register.filter
+def resource_target(resource):
+    if resource.__class__.__name__ == "AiLabExternalResource":
+        return "_blank"
+    else:
+        return "_self"
+
+
+@register.filter
 def link_to_resource_type(resource, page):
     slug = slugify(resource._meta.verbose_name)
     return page.url + page.reverse_subpage("filter_by_type", args=[slug])
