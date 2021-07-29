da836f550a24e074c8a771ce031c1ef1d6d56187
Dragon (David McKee) <dragon@dxw.com>
2021-07-29 11:41:57 +0100

    Customise config of FinderBlock backend

--- app/modules/finder/blocks.py
+++ app/modules/finder/blocks.py
@@ -1,36 +1,16 @@
 from wagtail.contrib.table_block.blocks import TableBlock 
-from wagtail.core.blocks import StreamBlock, StructBlock, CharBlock
+import logging
 
 """Docs for underlying TableBlock:
 https://docs.wagtail.io/en/stable/reference/contrib/table_block.html
 """
 
 class FinderBlock(TableBlock):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.table_options['minSpareRows'] = 1
+        self.table_options['startCols'] = 4
+        self.table_options['autoColumnSize'] = True
+
     class Meta:
         template = 'finder/blocks/finder_block.html'
-
-default_table_options = {
-    'minSpareRows': 1,
-    'startRows': 3,
-    'startCols': 4,
-    'colHeaders': False,
-    'rowHeaders': True,
-    'contextMenu': [
-        'row_above',
-        'row_below',
-        '---------',
-        'col_left',
-        'col_right',
-        '---------',
-        'remove_row',
-        'remove_col',
-        '---------',
-        'undo',
-        'redo'
-    ],
-    'editor': 'text',
-    'stretchH': 'all',
-    'height': 108,
-    'renderer': 'text',
-    'autoColumnSize': True,
-}
