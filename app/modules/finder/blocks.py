0e2c19fa6a63f497e9c370ee2db1a8539a5aa9b7
Dragon (David McKee) <dragon@dxw.com>
2021-07-29 11:24:11 +0100

    Remove old nested finder.

--- app/modules/finder/blocks.py
+++ app/modules/finder/blocks.py
@@ -5,44 +5,32 @@ from wagtail.core.blocks import StreamBlock, StructBlock, CharBlock
 https://docs.wagtail.io/en/stable/reference/contrib/table_block.html
 """
 
-class SearchableTableBlock(TableBlock):
+class FinderBlock(TableBlock):
     class Meta:
         template = 'finder/blocks/finder_block.html'
 
-class PersonBlock(StructBlock):
-    first_name = CharBlock()
-
-    class Meta:
-        icon = 'user'
-        form_classname = 'person-block struct-block'
-        template = 'finder/blocks/finder_js_block.html'
-
-class FinderBlock(StreamBlock):
-
-    default_table_options = {
-        'minSpareRows': 1,
-        'startRows': 3,
-        'startCols': 4,
-        'colHeaders': False,
-        'rowHeaders': True,
-        'contextMenu': [
-            'row_above',
-            'row_below',
-            '---------',
-            'col_left',
-            'col_right',
-            '---------',
-            'remove_row',
-            'remove_col',
-            '---------',
-            'undo',
-            'redo'
-        ],
-        'editor': 'text',
-        'stretchH': 'all',
-        'height': 108,
-        'renderer': 'text',
-        'autoColumnSize': True,
-    }
-    table=SearchableTableBlock(table_options=default_table_options)
-    person=PersonBlock()
+default_table_options = {
+    'minSpareRows': 1,
+    'startRows': 3,
+    'startCols': 4,
+    'colHeaders': False,
+    'rowHeaders': True,
+    'contextMenu': [
+        'row_above',
+        'row_below',
+        '---------',
+        'col_left',
+        'col_right',
+        '---------',
+        'remove_row',
+        'remove_col',
+        '---------',
+        'undo',
+        'redo'
+    ],
+    'editor': 'text',
+    'stretchH': 'all',
+    'height': 108,
+    'renderer': 'text',
+    'autoColumnSize': True,
+}
