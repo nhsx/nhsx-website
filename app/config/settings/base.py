6f3b11a15cf1b21e11f8b0dcb249348971c75b08
Dragon (David McKee) <dragon@dxw.com>
2021-09-07 17:50:54 +0100

    Increase password length to 10

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -247,6 +247,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {"min_length": 10},
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
