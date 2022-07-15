a4de4f7d98758916fa05ce139aa590f06d4a173b
David McKee <dragon@dxw.com>
2022-07-15 17:24:26 +0100

    Set WAGTAILADMIN_BASE_URL to suppress warning (and use transform)
    wagtailadmin.W003 The WAGTAILADMIN_BASE_URL setting is not defined
    HINT: This should be the base URL used to access the Wagtail admin site.
    Without this, URLs in notification emails will not display correctly.
    
    Documentation: https://docs.wagtail.org/en/latest/reference/settings.html#wagtailadmin-base-url

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -507,8 +507,8 @@ WAGTAIL_SITE_NAME = "nhsx"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://nhsx.test"
-
+WAGTAILADMIN_BASE_URL = "https://transform.england.nhs.uk"
+BASE_URL = WAGTAILADMIN_BASE_URL
 
 ####################################################################################################
 # Postmark
