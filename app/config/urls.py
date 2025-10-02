c7e62161e6567a78229530ae4a0f6dd575d74c2b
Sarah Mount <sarah.mount@dxw.com>
2025-10-02 15:20:55 +0100

    Add redirect for opensafely pages
    See: https://dxw.zendesk.com/agent/tickets/21285

--- app/config/urls.py
+++ app/config/urls.py
@@ -1094,6 +1094,14 @@ urlpatterns = [
             permanent=True,
         ),
     ),
+    # https://dxw.zendesk.com/agent/tickets/21285
+    url(
+        r"^key-tools-and-info/data-saves-lives/improving-care-through-research-and-innovation/opensafely-secure-access-to-data-to-deepen-our-understanding-of-covid-19/$",
+        lambda request: redirect(
+            r"/key-tools-and-info/data-saves-lives/improving-care-through-research-and-innovation/expansion-of-opensafely-secure-research-platform/",
+            permanent=True,
+        ),
+    ),
 ]
 
 
