6b8e40b971d68c1d1ad64ab5e3b245dcc40b3949
patdel0 <zefiru@protonmail.com>
2025-12-23 15:55:52 +0000

    Check for youtube markers in the iframe
    An approach was considered using Bs4 to correctly check the `src`
    attribute of the iframe, but that seemed too expensive for a middleware

--- app/modules/core/middleware.py
+++ app/modules/core/middleware.py
@@ -1,14 +1,29 @@
 class IframeReferrerMiddleware:
+    YOUTUBE_MARKERS = (
+        "youtube.com",
+        "youtu.be",
+    )
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
 
-        if "text/html" in response.get("Content-Type", ""):
-            content = response.content.decode("utf-8")
+        if "text/html" not in response.get("Content-Type", ""):
+            return response
 
-            if "<iframe" in content.lower():
-                response["Referrer-Policy"] = "no-referrer"
+        content = response.content.decode("utf-8").lower()
+        iframes = content.split("<iframe")
+        has_youtube_iframe = False
+
+        for iframe in iframes[1:]:
+            iframe_tag = iframe.split(">", 1)[0] + ">"
+            if any(marker in iframe_tag for marker in self.YOUTUBE_MARKERS):
+                has_youtube_iframe = True
+                break 
+
+        if has_youtube_iframe:
+            response["Referrer-Policy"] = "no-referrer"
 
         return response
